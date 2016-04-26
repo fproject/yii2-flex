d738df0738e888aeac9a086186d10ebe1f0862f5
Phan Van Thanh <thanhpv@fimo.edu.vn>
2016-04-26 11:33:57 +0700

    #3: Translation for Vietnamese

---
+++ fproject/translation/vi/flex.php
@@ -0,0 +1,28 @@
+<?php
+///////////////////////////////////////////////////////////////////////////////
+//
+// © Copyright f-project.net 2016. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+///////////////////////////////////////////////////////////////////////////////
+return [
+    'FlexWidget.name cannot be empty.' => 'FlexWidget.name không thể để trống.',
+    'FlexWidget.baseUrl cannot be empty.' => 'FlexWidget.baseUrl không thể để trống.',
+    'This content requires the {link}' => 'Nội dung này cần {link} để hiển thị.',
+    'To view this page ensure that Adobe Flash Player version {version} or greater is installed.' => 'Để hiển thị trang hãy chắc chắn phiên bản Adobe Flash Player {version} hoặc mới hơn được cài đặt.',
+    'Please follow this guide for {aTag} install Flash Player in five easy steps{aCloseTag}.' => 'Vui lòng làm theo hướng dẫn sau {aTag} để cài đặt Flash Player trong 5 bước đơn giản{aCloseTag}.',
+    'Or download it here' => 'Hoặc tải về trực tiếp tại đây ',
+    'Either scripts and active content are not permitted to run or Adobe Flash Player version {version} or greater is not installed.' => ''
+];
+?>
